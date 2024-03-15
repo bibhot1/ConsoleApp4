97b5c7569e2c329b5c98af73bbc865a6a721c9d5
Bibhot1 <bndazbvu.me12@gmail.com>
2024-03-16 08:08:23 +1300

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Nested if statements
+            Console.WriteLine("Enter your name");
+            bool isAdmin = false;
+            bool isRegistered = true;
+            string userName = "";
+            userName = Console.ReadLine();
+
+           /* if (isRegistered)
+            {
+                Console.WriteLine("Hi there, registered user");
+                if (userName != "")
+                {
+                    Console.WriteLine("Hi there, "+ userName);
+
+                    if (userName.Equals("Admin"))
+                    {
+                        Console.WriteLine("Hi there , Admin");
+                    }
+                }
+            }
+           */
+
+            // making the if statement simpler
+
+            if (isRegistered && userName != "" && userName.Equals("Admin"))
+            {
+                Console.WriteLine("Hi there, registered user");
+                Console.WriteLine("Hi there, " + userName);
+                Console.WriteLine("Hi there , Admin");
+            }
+            //Comparisions with or
+            if(isAdmin || isRegistered)
+            {
+                Console.WriteLine("You are logged in");
+            }
+
+        }
+    }
+}
